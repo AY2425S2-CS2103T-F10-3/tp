dadbc94fe72826e58160ff11a1ccb18e80cf41d6
cyrolite <e1122320@u.nus.edu>
2025-03-13 16:11:58 +0800

    Reformat HelpWindow.java

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -1,10 +1,10 @@
 package seedu.address.ui;
 
-import java.util.logging.Logger;
 import javafx.fxml.FXML;
+
+import java.util.logging.Logger;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 import javafx.stage.Stage;
