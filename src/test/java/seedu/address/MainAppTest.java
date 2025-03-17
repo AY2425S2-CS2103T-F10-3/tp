3692a150617e45a6bf685779322389d29197b595
pyy <45996894+pang-yy@users.noreply.github.com>
2025-03-17 22:50:39 +0800

    Rewrite test cases for MainApp.java

--- src/test/java/seedu/address/MainAppTest.java
+++ src/test/java/seedu/address/MainAppTest.java
@@ -4,16 +4,18 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
+import seedu.address.commons.exceptions.DataLoadingException;
+import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.ReadOnlyUserPrefs;
 import seedu.address.model.UserPrefs;
-import seedu.address.storage.JsonAddressBookStorage;
-import seedu.address.storage.JsonUserPrefsStorage;
-import seedu.address.storage.StorageManager;
+import seedu.address.storage.Storage;
 
 public class MainAppTest {
 
@@ -24,27 +26,103 @@ public class MainAppTest {
 
     @Test
     public void initModelManager_default() {
-        JsonAddressBookStorage addressBookStorage = new JsonAddressBookStorage(getTempFilePath("ab"));
-        JsonUserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(getTempFilePath("prefs"));
-        StorageManager storageManager = new StorageManager(addressBookStorage, userPrefsStorage);
+        Storage storageManager = new StorageManagerStub();
         assertFalse(mainApp.initModelManager(storageManager, new UserPrefs()).isInitialDataFileLoadingError());
     }
 
     @Test
     public void initModelManager_corruptedDataFile() throws Exception {
-        JsonAddressBookStorage addressBookStorage = new JsonAddressBookStorage(getCorruptedTempFilePath());
-        JsonUserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(getTempFilePath("prefs"));
-        StorageManager storageManager = new StorageManager(addressBookStorage, userPrefsStorage);
+        Storage storageManager = new StorageManagerErrorStub();
         assertTrue(mainApp.initModelManager(storageManager, new UserPrefs()).isInitialDataFileLoadingError());
     }
 
-    private Path getTempFilePath(String fileName) {
-        return testFolder.resolve(fileName);
+    /**
+     * A stub StorageManager.
+     */
+    private static class StorageManagerStub implements Storage {
+        @Override
+        public Path getUserPrefsFilePath() {
+            return Paths.get("testDir");
+        }
+
+        @Override
+        public Optional<UserPrefs> readUserPrefs() throws DataLoadingException {
+            return Optional.empty();
+        }
+
+        @Override
+        public Path getAddressBookFilePath() { // this one will be called
+            return Paths.get("testDir");
+        }
+
+        @Override
+        public Optional<ReadOnlyAddressBook> readAddressBook() throws DataLoadingException { // this one need to throw
+            return Optional.empty();
+        }
+
+        @Override
+        public Optional<ReadOnlyAddressBook> readAddressBook(Path filePath) throws DataLoadingException {
+            return Optional.empty();
+        }
+
+        @Override
+        public void saveAddressBook(ReadOnlyAddressBook addressBook, Path filePath) throws IOException {
+            throw new IOException();
+        }
+
+        @Override
+        public void saveAddressBook(ReadOnlyAddressBook addressBook) throws IOException {
+            throw new IOException();
+        }
+
+        @Override
+        public void saveUserPrefs(ReadOnlyUserPrefs userPrefs) throws IOException {
+            throw new IOException();
+        }
     }
 
-    private Path getCorruptedTempFilePath() throws IOException {
-        Path testWrongFormatFile = getTempFilePath("testContains123.txt");
-        Files.writeString(testWrongFormatFile, "123");
-        return testWrongFormatFile;
+    /**
+     * A stub StorageManager which will throw error.
+     */
+    private static class StorageManagerErrorStub implements Storage {
+        @Override
+        public Path getUserPrefsFilePath() {
+            return Paths.get("testDir");
+        }
+
+        @Override
+        public Optional<UserPrefs> readUserPrefs() throws DataLoadingException {
+            return Optional.empty();
+        }
+
+        @Override
+        public Path getAddressBookFilePath() { // this one will be called
+            return Paths.get("testDir");
+        }
+
+        @Override
+        public Optional<ReadOnlyAddressBook> readAddressBook() throws DataLoadingException { // this one need to throw
+            throw new DataLoadingException(new Exception("error"));
+        }
+
+        @Override
+        public Optional<ReadOnlyAddressBook> readAddressBook(Path filePath) throws DataLoadingException {
+            throw new DataLoadingException(new Exception("error"));
+        }
+
+        @Override
+        public void saveAddressBook(ReadOnlyAddressBook addressBook, Path filePath) throws IOException {
+            throw new IOException();
+        }
+
+        @Override
+        public void saveAddressBook(ReadOnlyAddressBook addressBook) throws IOException {
+            throw new IOException();
+        }
+
+        @Override
+        public void saveUserPrefs(ReadOnlyUserPrefs userPrefs) throws IOException {
+            throw new IOException();
+        }
     }
 }
