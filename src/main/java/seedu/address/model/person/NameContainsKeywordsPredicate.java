fbe2ec0c1ef953fc38cdc1332dbd43ad90936bb9
Melvan-Chan <42329954+Melvan-Chan@users.noreply.github.com>
2025-03-19 22:40:44 +0800

    Update search function to search by tags

--- src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
@@ -22,6 +22,8 @@ public class NameContainsKeywordsPredicate implements Predicate<Person> {
                 StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword)
                 || StringUtil.containsWordIgnoreCase(person.getEmail().value, keyword)
                 || StringUtil.containsWordIgnoreCase(person.getPhone().value, keyword)
+                || person.getTags().stream()
+                .anyMatch(tag -> StringUtil.containsWordIgnoreCase(tag.tagName, keyword))
         );
     }
 
