715593546724ef0182beb8ad07c52170696b5ca5
iamanoob44 <e1121904@u.nus.edu>
2025-03-27 02:29:56 +0800

    Add test case for more comprehensive testing

--- src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -77,6 +78,27 @@ public class DeleteCommandTest {
         DeleteCommand deleteCommand = new DeleteCommand(outOfBoundIndex, true);
 
         assertCommandFailure(deleteCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_validIndex_deletePersonSuccessfully() {
+        Person personToDelete = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        DeleteCommand deleteCommand = new DeleteCommand(INDEX_FIRST_PERSON, true);
+
+        String expectedSuccessMessage = String.format(DeleteCommand.MESSAGE_DELETE_PERSON_SUCCESS,
+                Messages.format(personToDelete));
+
+        ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.deletePerson(personToDelete);
+
+        try {
+            CommandResult result = deleteCommand.execute(model);
+            assertEquals(expectedSuccessMessage, result.getFeedbackToUser());
+            assertEquals(expectedModel, model);
+        } catch (CommandException e) {
+            // Dummy statement , but won't reach this line since we will always delete successfully for valid indexes
+            throw new RuntimeException("Error occured from DeleteCommandTest");
+        }
     }
 
     @Test
