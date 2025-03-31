ece79cb557ce8242f83a136696307ab266e92437
cyrolite <e1122320@u.nus.edu>
2025-03-31 19:50:12 +0800

    Add test cases for CommandItem.java

--- src/main/java/seedu/address/ui/CommandItem.java
+++ src/main/java/seedu/address/ui/CommandItem.java
@@ -1,5 +1,7 @@
 package seedu.address.ui;
 
+import java.util.Objects;
+
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
@@ -60,5 +62,23 @@ public class CommandItem {
      */
     public StringProperty getFormatProperty() {
         return format;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (!(other instanceof CommandItem)) {
+            return false;
+        }
+        CommandItem otherCommandItem = (CommandItem) other;
+        return this.title.get().equals(otherCommandItem.title.get())
+                && this.format.get().equals(otherCommandItem.format.get());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, format);
     }
 }
