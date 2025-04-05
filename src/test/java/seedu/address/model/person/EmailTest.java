523d08561e6d5cf6af9df8b9b8dc1e4a12354c3d
cyrolite <e1122320@u.nus.edu>
2025-04-05 21:04:50 +0800

    Update EmailTest.java

--- src/test/java/seedu/address/model/person/EmailTest.java
+++ src/test/java/seedu/address/model/person/EmailTest.java
@@ -51,6 +51,8 @@ public class EmailTest {
         assertFalse(Email.isValidEmail("peterjack@-example.com")); // domain name starts with a hyphen
         assertFalse(Email.isValidEmail("peterjack@example.com-")); // domain name ends with a hyphen
         assertFalse(Email.isValidEmail("peterjack@example.c")); // top level domain has less than two chars
+        assertFalse(Email.isValidEmail("randompersonsverysuperdupersuperquadruplelongnamewithavery"
+                + "surprisinglylongname@examplewithaverysurprisinglongdomain.com")); // very long email
 
         // valid email
         assertTrue(Email.isValidEmail("PeterJack_1190@example.com")); // underscore in local part
